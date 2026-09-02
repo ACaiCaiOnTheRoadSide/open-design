9961965de857d2c6a1a3888a45829565265c53a3
qingjing.cai <qingjing.cai@chaitin.com>
2026-09-02 15:51:57 +0800

    feat(web): update OhMyDesign wordmark

--- apps/web/app/layout.tsx
+++ apps/web/app/layout.tsx
@@ -7,7 +7,7 @@ import '../src/index.css';
 import '../src/styles/home/index.css';
 
 export const metadata: Metadata = {
-  title: 'OpenDesign',
+  title: 'OhMyDesign',
   icons: {
     icon: '/app-icon.png',
     apple: '/app-icon.png',
